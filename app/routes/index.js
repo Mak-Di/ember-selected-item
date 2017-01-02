3d6ab882523ed6032ad923e964a7e2be4012267f
Dmitriy Makarov <makarov.dmitriy@gmail.com>
2017-01-02 12:41:52 +0000

    Add simple items

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [
+      {id: '1', name: 'item 1'},
+      {id: '2', name: 'item 2'},
+      {id: '3', name: 'item 3'},
+    ];
+  }
+});
